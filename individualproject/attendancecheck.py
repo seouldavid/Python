750c74fcb557c84cda9c9b7304ab3327e20818b1
seouldavid <seouldavid98@gmail.com>
2023-01-18 21:05:48 +0800

    Update attendancecheck.py

--- individualproject/attendancecheck.py
+++ individualproject/attendancecheck.py
@@ -1,3 +1,16 @@
-# retrieve date come, out
+from datetime import datetime
+
+now = datetime.now()
+attendanceBook = {}
 # retrieve name
-# store in dictionary 
+name = input("Enter your name: (for exit, press q)")
+while (name != "q"):
+    currTime = now.strftime("%m/%d/%Y, %H:%M:%S")
+    if name in attendanceBook:
+        attendanceBook[name] += " " + currTime + "(out)"
+        print(attendanceBook)
+    else:
+        attendanceBook[name] = currTime + "(in)"
+        print(attendanceBook)
+    name = input("Enter your name: (for exit, press q)")
+print("\nExit\n")
