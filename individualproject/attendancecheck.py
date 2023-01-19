4c7b3ac54baf74330104a1ef44f81cadb5dc7527
seouldavid <seouldavid98@gmail.com>
2023-01-19 17:02:46 +0800

    Update attendancecheck.py

--- individualproject/attendancecheck.py
+++ individualproject/attendancecheck.py
@@ -7,10 +7,15 @@ name = input("Enter your name: (for exit, press q)")
 while (name != "q"):
     currTime = now.strftime("%m/%d/%Y, %H:%M:%S")
     if name in attendanceBook:
-        attendanceBook[name] += " " + currTime + "(out)"
+        if (attendanceBook[name][-1][attendanceBook[name][-1].find("(")+1:attendanceBook[name][-1].find(")")] == "in"):
+            attendanceBook[name].append(" " + currTime + "(out)")
+        else:
+            attendanceBook[name].append(" " + currTime + "(in)")
+
         print(attendanceBook)
     else:
-        attendanceBook[name] = currTime + "(in)"
+        attendanceBook[name] = []
+        attendanceBook[name].append(currTime + "(in)")
         print(attendanceBook)
     name = input("Enter your name: (for exit, press q)")
 print("\nExit\n")
